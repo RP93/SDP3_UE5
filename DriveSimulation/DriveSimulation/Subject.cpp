3dcf1545feaeaab130c4d67f3066cbe1db6bc29d
Reinhard <reinhard93@gmx.at>
2012-12-04 17:02:32 +0100

    17:05
    Subject implemented

--- DriveSimulation/DriveSimulation/Subject.cpp
+++ DriveSimulation/DriveSimulation/Subject.cpp
@@ -5,16 +5,37 @@
 // Description : Implementation of class Subject
 ///////////////////////////////////////////////////////////////////////////
 
+#include <algorithm>
 #include "Subject.h"
 
-void Subject::Attach(IObserver*)
+void Subject::Attach(IObserver* ob)
 {
+	//searches observer in observer list
+	TObserversConstItor found =
+		std::find(mObservers.begin(),
+		mObservers.end(),ob);
+	if (found == mObservers.end()){
+		//observer not found in list
+		mObservers.push_back(ob) ;
+	}
 }
 
-void Subject::Detach(IObserver*)
+void Subject::Detach(IObserver* ob)
 {
+	//searches observer in observer list
+	TObserversConstItor foundObserver =
+		find(mObservers.begin(),
+		mObservers.end(), ob);
+	if( foundObserver != mObservers.end() ) {
+		//observer found in list
+		mObservers.erase(foundObserver) ;
+	}
 }
 
 void Subject::NotifyObservers()
 {
+	std::for_each(mObservers.begin(),mObservers.end(),[](IObserver* first)
+	{
+		first->Update();
+	});
 }
--- DriveSimulation/DriveSimulation/Subject.h
+++ DriveSimulation/DriveSimulation/Subject.h
@@ -11,17 +11,23 @@
 #include "Object.h"
 #include "IObserver.h"
 #include <list>
+#include <iterator>
 
 typedef std::list<IObserver*> TObservers;
+typedef TObservers::const_iterator TObserversConstItor;
 
 class Subject :
 	public Object
 {
 public:
-	void Attach(IObserver*);
-	void Detach(IObserver*);
+	//adds an observer to observer list
+	void Attach(IObserver* ob);
+
+	//removes an observer from observer list
+	void Detach(IObserver* ob);
 
 protected:
+	//notify all observers in observer list
 	void NotifyObservers();
 
 private:
